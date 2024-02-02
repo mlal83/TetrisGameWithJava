b6d0fa13a60cfa1b269a056c26e60bf8979b04e9
Manjula Lal <manjula_lal@hotmail.com>
2024-02-02 14:07:13 +0000

    added design

--- assets/java/script.js
+++ assets/java/script.js
@@ -1,3 +1,46 @@
+/*const apiKey = "8229f396";
+
+const buttonElement = document.getElementById("button");
+const inputElement = document.getElementById("input");
+const resultArea = document.getElementById("result-area");
+
+function handleButtonClick() {
+    let movieTitle = inputElement.value;
+    let url = `https://www.omdbapi.com/?t=${movieTitle}&apikey=${apiKey}`;
+    fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+        showFilmData(data);
+    });
+}
+
+buttonElement.addEventListener('click', handleButtonClick);
+
+function showFilmData(data) {
+    let title = data.Title;
+    let year = data.Year;
+    let director = data.Director;
+    let poster = data.Poster;
+    let genre = data.Genre;
+    let runtime = data.Runtime;
+    let cast = data.Actors;
+    let plot = data.Plot;
+    let imdbRating = data.imdbRating;
+    let htmlString = `
+    <h1>${title}</h1>
+    <h4>${year}, ${director}</h4>
+    <img src="${poster}">
+    <p>${genre}; ${runtime}</p>
+    <p>Starring: ${cast}</p>
+    <p>${plot}</p>
+    <p>IMDb Rating: ${imdbRating}</p>
+    `;
+    resultArea.innerHTML = htmlString;
+}
+*/
+
 const Question = [
   {
     question: "Which element has the chemical symbol 'Ir'?",
